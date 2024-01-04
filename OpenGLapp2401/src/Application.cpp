c47772273f9b9196c25181eac062f7086fcab6a9
SafakOnol <onolsafak@outlook.com>
2024-01-03 20:54:13 -0500

    add Vertex Attributes

--- OpenGLapp2401/src/Application.cpp
+++ OpenGLapp2401/src/Application.cpp
@@ -29,15 +29,21 @@ int main(void)
 
     float positions[6] =
     {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+        -0.5f, -0.5f, // vertex1 pos
+         0.0f,  0.5f, // vertex2 pos
+         0.5f, -0.5f  // vertex3 pos
     };
+
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
